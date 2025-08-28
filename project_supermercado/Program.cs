b91175e95f1548a9e86084c3fa5252c65fc91102
VRMOTA <w0lf3ryxd@gmail.com>
2025-08-27 23:50:10 -0600

    asddsa

--- project_supermercado/Program.cs
+++ project_supermercado/Program.cs
@@ -16,7 +16,7 @@ namespace project_supermercado
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_login());
+            Application.Run(new Form1());
         }
     }
 }
